3f52a170ccb3e69e1d1f4430bbfa6c5bb038465a
ravenbashdar <75314649+ravenbashdar@users.noreply.github.com>
2021-01-14 05:51:30 +0300

    Arez.sh

---
+++ Arez.sh
@@ -0,0 +1,37 @@
+
+#!/bin/bash
+clear
+
+ cd ..
+ rm -rif usr/etc/motd
+ cd $HOME
+
+printf "\033[91m#############<< Arez>>#############\n\r"
+printf "\033[92m#      tish Tool create By        #\n\r"
+printf "\033[92m#           Arez bashdar          #\n\r"
+printf "\033[92m#        snapchat {RavenFon}      #\n\r"
+printf "\033[91m#############<<Arez >>#############\n\r"
+sleep 3
+xdg-open https://t.me/Arezbahsdar
+pkg update -y
+clear
+pkg upgrade
+sleep 10
+clear
+printf "\033[92m##########<<    Arez   >>##########\n\r"
+printf "\033[91m#   Chawre bka ta command dabzet  #\n\r"
+printf "\033[91m#      Snapchat {RavenFon}        #\n\r"
+printf "\033[92m##########<<   Arez    >>##########\n\r"
+sleep pkg update -y;pkg upgrade -y;pkg install python -y;pkg install python2 -y;pkg install python2-dev -y;pkg install python3 -y;pkg install java -y;pkg install fish -y;pkg install ruby -y;pkg install help -y;pkg install git -y;pkg install host -y;pkg install php -y;pkg install perl -y;pkg install nmap -y;pkg install bash -y;pkg install clang -y;pkg install nano -y;pkg install w3m -y;pkg install havij -y;pkg install hydra -y;pkg install figlet -y;pkg install cowsay -y;pkg install curl -y;pkg install tar -y;pkg install zip -y;pkg install unzip -y;pkg install tor -y;pkg install google -y;pkg install sudo -y;pkg install wget -y;pkg install wireshark -y;pkg install wgetrc -y;pkg install wcalc -y;pkg install bmon -y;pkg install vpn -y;pkg install unrar -y;pkg install toilet -y;pkg install proot -y;pkg install net-tools -y;figlet profisor;pkg install golang -y;pkg install chroot -y;termux-chroot -y;pkg install macchanger -y;pkg install openssl -y;pkg install cmatrix -y;pkg install openssh -y;figlet Arez Bashdar
+clear
+figlet snapchat
+xdg-open https://www.snapchat.com/add/RavenFon
+sleep 10
+clear
+printf "\033[91m#############<<Arezm>>#############\n\r"
+printf "\033[92m#        This Tool Crate BY       #\n\r"
+printf "\033[92m#            Arez Bashdar         #\n\r"
+printf "\033[92m#        snapchat {RavenFon}   #\n\r"
+printf "\033[91m#############<<Arezm>>#############\n\r"
+sleep 4
+figlet Tawaw kar aka
